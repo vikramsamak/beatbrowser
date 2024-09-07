81fe8a3ac1f33223f2148d44a486e03af10adc3c
vikramsamak <vikramsamak02@gmail.com>
2024-09-07 08:37:59 +0530

    Added constants.

---
+++ src/constants/constants.ts
@@ -0,0 +1,5 @@
+export const SPOTIFY_API_TOKEN_URL = "https://accounts.spotify.com/api/token";
+export const SPOTIFY_API_SEARCH_URL = "https://api.spotify.com/v1/search";
+export const SPOTIFY_API_ARTIST_URL = "https://api.spotify.com/v1/artists";
+export const SEARCH_BY_ARTIST_TYPE = "ARTIST";
+export const SEARCH_BY_SONG_TITLE_TYPE = "SONG_TITLE";
